86afeea2ab411409ea91b73a2559cb4f6c88afa8
YYYutang <1036503272@qq.com>
2023-03-24 22:24:10 +0800

    第三次

--- src/components/tab/connectMining.vue
+++ src/components/tab/connectMining.vue
@@ -1,71 +1,149 @@
 <template>
-<div>
-<el-header>
-<h3 >任务设置</h3>
-<el-divider></el-divider>
-</el-header>
+  <div>
+    <el-header>
+      <h3>任务设置</h3>
+      <el-divider></el-divider>
+    </el-header>
 
-<el-container id="maintest">
-<div id="main">
-<el-form ref="form" :model="form" label-width="80px"  v-show="showForm">
-  <el-form-item label="任务名称">
-    <el-input v-model="form.name"></el-input>
-  </el-form-item>
-   <el-form-item label="任务描述">
-    <el-input v-model="form.description"></el-input>
-  </el-form-item>
-   <el-form-item label="选择病种">
-  <el-select v-model="value1" multiple placeholder="请选择">
-    <el-option
-      v-for="item in diseaseOptions"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select>
-   </el-form-item>
-<el-form-item label="选择病种的指标">
-  <el-select
-    v-model="value2"
-    multiple
-    collapse-tags
-    style="margin-left: 20px;"
-    placeholder="请选择">
-    <el-option
-      v-for="item in diseaseInOptions"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select>
-  </el-form-item>
-  <el-form-item  label="使用模型">
-    <el-select v-model="form.model" placeholder="请选择使用模型">
-      <el-option label="apriori" value="apriori"></el-option>
-      <el-option label="svm" value="svm"></el-option>
-      <el-option label="xgboost" value="xgboost"></el-option>
-      <el-option label="knn" value="knn"></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item class="buttonDM" id="button1">
-    <el-button type="primary" @click="onSubmitDM">运行</el-button>
-  </el-form-item>
-</el-form>
-</div>
-<div id="charts1">
-<div id="chart1" style="width:600px; height: 400px" v-show="showChart" @click="zoomout">
-</div>
-<div id="chart2" style="width: 800px; height: 400px " v-show="showChart">
-</div>
-</div>
-<div id="charts2">
-<div id="chart3" style="width: 600px; height: 400px " v-show="showChart">
-</div>
-<div id="chart4" style="width: 600px; height: 400px " v-show="showChart">
-</div>
-</div>
-</el-container>
-</div>
+    <el-container id="maintest">
+      <div id="main">
+        <el-form ref="form" :model="form" label-width="120px" v-show="showForm">
+          <el-form-item label="任务名称">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="任务描述">
+            <el-input v-model="form.description"></el-input>
+          </el-form-item>
+          <el-form-item label="选择病种">
+            <el-select v-model="value1" multiple placeholder="请选择">
+              <el-option
+                v-for="item in diseaseOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="选择病种的指标">
+            <el-select
+              v-model="value2"
+              multiple
+              collapse-tags
+              placeholder="请选择"
+            >
+              <el-option
+                v-for="item in diseaseInOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="使用模型">
+            <el-select v-model="form.model" placeholder="请选择使用模型">
+              <el-option label="apriori" value="apriori"></el-option>
+              <el-option label="svm" value="svm"></el-option>
+              <el-option label="xgboost" value="xgboost"></el-option>
+              <el-option label="knn" value="knn"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item class="buttonDM" id="button1">
+            <el-button type="primary" @click="onSubmitDM">运行</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div id="charts1" v-show="showChart">
+          <div id="inlineForm">
+        <el-form :inline="true" :model="formInline" class="demo-form-inline">
+          <el-form-item label="病种1">
+            <el-select
+              v-model="formInline.value3"
+              placeholder="请选择"
+              style="width: 100px"
+            >
+              <el-option
+                v-for="item in chartOneOptionsOne"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="病种1的指标">
+            <el-select
+              v-model="formInline.value4"
+              placeholder="请选择"
+              style="width: 100px"
+            >
+              <el-option
+                v-for="item in chartOneOptionsTwo"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+        <el-form-item label="病种2">
+          <el-select
+            v-model="formInline.value5"
+            placeholder="请选择"
+            style="width: 100px"
+          >
+            <el-option
+              v-for="item in chartOneOptionsOne"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="病种2的指标">
+          <el-select
+            v-model="formInline.value6"
+            placeholder="请选择"
+            style="width: 100px"
+          >
+            <el-option
+              v-for="item in chartOneOptionsTwo"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        </el-form>
+          </div>
+        <div
+          id="chart1"
+          style="width: 600px; height: 400px"
+          v-show="showChart"
+        ></div>
+        <div
+          id="chart2"
+          style="width: 800px; height: 400px"
+          v-show="showChart"
+        ></div>
+      </div>
+      <div id="charts2">
+        <div
+          id="chart3"
+          style="width: 600px; height: 400px"
+          v-show="showChart"
+        ></div>
+        <div
+          id="chart4"
+          style="width: 600px; height: 400px"
+          v-show="showChart"
+        ></div>
+      </div>
+    </el-container>
+  </div>
 </template>
 <script>
 export default {
@@ -100,8 +178,8 @@ export default {
           label: "阿尔兹海默症",
         },
       ],
-      diseaseInOptions:[
-          {
+      diseaseInOptions: [
+        {
           value: "选项1",
           label: "血脂",
         },
@@ -118,8 +196,53 @@ export default {
           label: "年龄",
         },
       ],
+      chartOneOptionsOne: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
+      chartOneOptionsTwo: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
       value1: [],
-        value2: []
+      value2: [],
+      formInline: { value3: "", value4: "",value5:"",value6:"" },
     };
   },
   methods: {
@@ -141,17 +264,11 @@ export default {
       let myChart3 = this.$echarts.init(document.getElementById("chart3"));
       let myChart4 = this.$echarts.init(document.getElementById("chart4"));
       // 指定图表的配置项和数据
+      //散点图
       let option1 = {
         xAxis: {},
         yAxis: {},
-        toolbox: {
-          feature: {
-            dataZoom: { yAxisIndex: "none" }, // 数据区域缩放
-            restore: { show: true }, // 重置
-            saveAsImage: { show: true }, // 导出图片
-          },
-        },
-        color: "rgba(128,128,255,0.8)",
+        color: "#75AAF2",
         series: [
           {
             symbolSize: 20,
@@ -185,19 +302,23 @@ export default {
       };
       const disease1 = ["高血压", "糖尿病", "肺癌", "胃癌", "阿尔兹海默症"];
       // prettier-ignore
-      const disease2 = [
-    '高血压', '糖尿病', '肺癌', '胃癌', '阿尔兹海默症'
-];
+      const disease2 = ['高血压', '糖尿病', '肺癌', '胃癌', '阿尔兹海默症'];
       // prettier-ignore
-      const data = [[0, 0, 5], [0, 1, 1], [0, 2, 0], [0, 3, 0], [0, 4, 0],  [1, 1, 0], [1, 2, 0], [1, 3, 0], [1, 4, 0],  [2, 0, 1], [2, 1, 1], [2, 2, 0], [2, 3, 0], [2, 4, 0],  [3, 1, 3], [3, 2, 0], [3, 3, 0], [3, 4, 0], [4, 0, 1], [4, 1, 3], [4, 2, 0], [4, 3, 0], [4, 4, 0]]
+      const data = [[0, 0, 0], [0, 1, 1], [0, 2, 1], [0, 3, 0], [0, 4, 1],  [1, 0, 1], [1, 2, 1], [1, 3, 3], [1, 4, 3],  [2, 0, 1], [2, 1, 1], [2, 2, 0], [2, 3, 1], [2, 4, 1],  
+      [3, 0, 0], [3, 1, 3], [3, 2, 1], [3, 4, 0], [4, 0, 1], [4, 1, 3], [4, 2, 1], [4, 3, 0], [4, 4, 0]]
     .map(function (item) {
     return [item[1], item[0], item[2] || '-'];
 });
+      //热力图
       let option2 = {
         tooltip: {
-          position: "top",
+          trigger: "item",
+          formatter: function (params) {
+            const d1 = disease1[params.data[1]];
+            const level = params.data[2];
+            return d1 + "与" + params.name + "的关联度为" + level;
+          },
         },
-
         grid: {
           height: "50%",
           top: "10%",
@@ -224,6 +345,7 @@ export default {
           left: "center",
           bottom: "15%",
         },
+
         series: [
           {
             name: "Punch Card",
@@ -241,9 +363,10 @@ export default {
           },
         ],
       };
+      //饼状图
       let option3 = {
         title: {
-          text: "Referer of a Website",
+          text: "分布图",
           subtext: "Fake Data",
           left: "center",
         },
@@ -256,15 +379,17 @@ export default {
         },
         series: [
           {
-            name: "Access From",
+            name: "数据分布图",
             type: "pie",
             radius: "50%",
             data: [
-              { value: 1048, name: "Search Engine" },
-              { value: 735, name: "Direct" },
-              { value: 580, name: "Email" },
-              { value: 484, name: "Union Ads" },
-              { value: 300, name: "Video Ads" },
+              { value: 1048, name: "0-20" },
+              { value: 735, name: "20-30" },
+              { value: 580, name: "30-40" },
+              { value: 484, name: "40-50" },
+              { value: 300, name: "50-60" },
+              { value: 200, name: "60-70" },
+              { value: 121, name: "70-80" },
             ],
             emphasis: {
               itemStyle: {
@@ -276,16 +401,15 @@ export default {
           },
         ],
       };
+      //关系图
       let option4 = {
-        tooltip: {},
         animationDurationUpdate: 1500,
         animationEasingUpdate: "quinticInOut",
         series: [
           {
             type: "graph",
             layout: "none",
-            symbolSize: 50,
-            roam: true,
+            symbolSize: 70,
             label: {
               show: true,
             },
@@ -296,23 +420,28 @@ export default {
             },
             data: [
               {
-                name: "Node 1",
+                name: "高血压",
                 x: 300,
                 y: 300,
               },
               {
-                name: "Node 2",
+                name: "胃癌",
                 x: 800,
                 y: 300,
               },
               {
-                name: "Node 3",
+                name: "肺癌",
                 x: 550,
                 y: 100,
               },
               {
-                name: "Node 4",
-                x: 550,
+                name: "阿尔兹海默症",
+                x: 450,
+                y: 500,
+              },
+              {
+                name: "糖尿病",
+                x: 650,
                 y: 500,
               },
             ],
@@ -322,39 +451,45 @@ export default {
                 source: 0,
                 target: 1,
                 symbolSize: [5, 20],
-                label: {
-                  show: true,
-                },
                 lineStyle: {
                   width: 5,
                   curveness: 0.2,
                 },
               },
               {
-                source: "Node 2",
-                target: "Node 1",
-                label: {
-                  show: true,
-                },
+                source: "胃癌",
+                target: "高血压",
                 lineStyle: {
                   curveness: 0.2,
                 },
               },
               {
-                source: "Node 1",
-                target: "Node 3",
+                source: "高血压",
+                target: "肺癌",
               },
               {
-                source: "Node 2",
-                target: "Node 3",
+                source: "胃癌",
+                target: "肺癌",
               },
               {
-                source: "Node 2",
-                target: "Node 4",
+                source: "胃癌",
+                target: "阿尔兹海默症",
               },
               {
-                source: "Node 1",
-                target: "Node 4",
+                source: "高血压",
+                target: "阿尔兹海默症",
+              },
+              {
+                source: "糖尿病",
+                target: "高血压",
+              },
+              {
+                source: "糖尿病",
+                target: "胃癌",
+              },
+              {
+                source: "阿尔兹海默症",
+                target: "糖尿病",
               },
             ],
             lineStyle: {
@@ -388,5 +523,8 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+#inlineForm{
+    margin-top:50px;
 }
 </style> 
--- src/components/tab/dangePredict.vue
+++ src/components/tab/dangePredict.vue
@@ -1,57 +1,73 @@
 <template>
-<div>
-<el-header>
-<h3 >任务设置</h3>
-<el-divider></el-divider>
-</el-header>
+  <div>
+    <el-header>
+      <h3>任务设置</h3>
+      <el-divider></el-divider>
+    </el-header>
 
-<el-container id="maintest">
-<div id="main">
-<el-form ref="form" :model="form" label-width="80px"  v-show="showForm">
-  <el-form-item label="任务名称">
-    <el-input v-model="form.name"></el-input>
-  </el-form-item>
-   <el-form-item label="任务描述">
-    <el-input v-model="form.description"></el-input>
-  </el-form-item>
-<el-form-item label="选择指标">
-  <el-select
-    v-model="value2"
-    multiple
-    collapse-tags
-    style="margin-left: 20px;"
-    placeholder="请选择">
-    <el-option
-      v-for="item in diseaseInOptions"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select>
-  </el-form-item>
-  <el-form-item  label="使用模型">
-    <el-select v-model="form.model" placeholder="请选择使用模型">
-      <el-option label="apriori" value="apriori"></el-option>
-      <el-option label="svm" value="svm"></el-option>
-      <el-option label="xgboost" value="xgboost"></el-option>
-      <el-option label="knn" value="knn"></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item class="buttonDM" id="button1">
-    <el-button type="primary" @click="onSubmitDM">运行</el-button>
-  </el-form-item>
-</el-form>
-</div>
-<div id="chart1" style="width:600px; height: 400px" v-show="showChart" >
-</div>
-</el-container>
-</div>
+    <el-container id="maintest">
+      <div id="main">
+        <el-form ref="form" :model="form" label-width="80px" v-show="showForm">
+          <el-form-item label="任务名称">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="任务描述">
+            <el-input v-model="form.description"></el-input>
+          </el-form-item>
+          <el-form-item label="选择指标">
+            <el-select
+              v-model="value2"
+              multiple
+              collapse-tags
+              placeholder="请选择"
+            >
+              <el-option
+                v-for="item in diseaseInOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="使用模型">
+            <el-select v-model="form.model" placeholder="请选择使用模型">
+              <el-option label="apriori" value="apriori"></el-option>
+              <el-option label="svm" value="svm"></el-option>
+              <el-option label="xgboost" value="xgboost"></el-option>
+              <el-option label="knn" value="knn"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item class="buttonDM" id="button1">
+            <el-button type="primary" @click="onSubmitDM">运行</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div id ="charts">
+        <div
+          id="chart1"
+          style="width: 600px; height: 400px"
+          v-show="showChart"
+        ></div>
+        <div
+          id="chart2"
+          style="width: 700px; height: 400px"
+          v-show="showChart"
+        ></div>
+      
+      <div
+        id="chart3"
+        style="width: 700px; height: 400px"
+        v-show="showChart"
+      ></div>
+      </div>
+    </el-container>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
-
       showForm: true,
       showChart: false,
       form: {
@@ -60,8 +76,8 @@ export default {
         model: "",
       },
 
-      diseaseInOptions:[
-          {
+      diseaseInOptions: [
+        {
           value: "选项1",
           label: "血脂",
         },
@@ -79,48 +95,124 @@ export default {
         },
       ],
       value1: [],
-        value2: []
-    }
-    
+      value2: [],
+    };
   },
   methods: {
-   onSubmitDM(){
-        this.showForm = !this.showForm;
+    onSubmitDM() {
+      this.showForm = !this.showForm;
       this.showChart = !this.showChart;
-   },
-   drawChart() {
-       let myChart1 = this.$echarts.init(document.getElementById("chart1"));
-       let option1 = {
-  xAxis: {
-    type: 'category',
-
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  series: [
-    {
-      data: [820, 932, 901, 934, 1290, 1330, 1320],
-      type: 'line',
-      smooth: true,
-      areaStyle: {}
+    },
+    drawChart() {
+      let myChart1 = this.$echarts.init(document.getElementById("chart1"));
+      let myChart2 = this.$echarts.init(document.getElementById("chart2"));
+      let myChart3 = this.$echarts.init(document.getElementById("chart3"));
+      let option1 = {
+        title: {
+          text: "ROC曲线图",
+          left: "center",
+        },
+        xAxis: {
+          type: "category",
+          boundaryGap: false,
+          data: [0, 0.2, 0.4, 0.6, 0.8, 1],
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            data: [0, 0.5, 0.7, 0.8, 0.9, 0.91, 0.92],
+            type: "line",
+            areaStyle: {},
+          },
+          {
+            data: [0, 0.2, 0.4, 0.5, 0.6, 0.7, 0.8],
+            type: "line",
+            smooth: true,
+          },
+        ],
+        color: "#75AAF2",
+      };
+      let option2 = {
+        title: {
+          text: "Precision-Recall分析图",
+          left: "center",
+        },
+     tooltip: {
+          trigger: "axis",
+          formatter: "Recall：{b} <br>Precision: {c}%",
+        },
+        xAxis: {
+          name: "Recall",
+          type: "category",
+          boundaryGap: false,
+          data: [0, 0.2, 0.4, 0.6, 0.8, 1],
+        },
+        yAxis: {
+          name: "Precision",
+          type: "value",
+        },
+        series: [
+          {
+            data: [0.4, 0.5, 0.44, 0.6, 0.65, 0.66, 0.64],
+            type: "line",
+          },
+        ],
+        color: "#75AAF2",
+      };
+      let option3 = {
+        title: {
+          text: "肺癌及相关疾病患病几率",
+          left: "center",
+        },
+        tooltip: {
+          trigger: "axis",
+          formatter: "患病几率 : <br/>{b} : {c}",
+        },
+        xAxis: {
+          type: "category",
+          data: [
+            "胃癌",
+            "脑卒中",
+            "高血压",
+            "高血压",
+            "前列腺癌",
+            "糖尿病",
+            "肺癌",
+          ],
+          axisLabel: {
+      formatter: '{value} '
+    },
+        },
+        yAxis: {
+          type: "value",
+          data: [0, 20, 40, 60, 80, 100],
+          axisLabel: {
+      formatter: '{value} %'
     }
-  ]
-};
+        },
+        series: [
+          {
+            data: [40, 20, 15, 80, 70, 10, 70],
+            type: "bar",
+          },
+        ],
+        color: "#75AAF2",
+      };
 
-myChart1.setOption(option1);
-   }
+      myChart1.setOption(option1);
+      myChart2.setOption(option2);
+      myChart3.setOption(option3);
+    },
   },
   mounted() {
     this.drawChart();
   },
-  
 };
 </script>
 
 <style scoped>
-
 #maintest {
   display: flex;
   justify-content: center;
@@ -131,5 +223,10 @@ myChart1.setOption(option1);
   justify-content: center;
   align-items: center;
 }
+#charts{
 
+  justify-content: center;
+  align-items: center;
+
+}
 </style>
